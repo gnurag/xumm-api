5299a03b1125ce7e05cd7a98ae4555e1e80b3496
Wietse Wind <mail@wietse.com>
2020-05-01 13:22:39 +0200

    Allow config module without Express env

--- src/middleware/config.js
+++ src/middleware/config.js
@@ -6,30 +6,42 @@ const config = {
   mode: process.env.NODE_ENV || 'development'
 }
 
+const opts = {
+  loaded: false
+}
+
 module.exports = async function (expressApp) {
   let loadedConfig = {}
   
-  await new Promise((resolve, reject) => {
-    fs.readFile(path.resolve(__dirname, `../../${config.mode}.json`), (err, data) => {
-      if (err) {
-        reject(new Error('CONFIG FILE ERROR:', err.toString()))
-      } else {
-        try {
-          loadedConfig = JSON.parse(data.toString('utf8'))
-          resolve(loadedConfig)
-        } catch (e) {
-          reject(new Error('CONFIG FILE PARSE ERROR', e.toString()))
+  if (!opts.loaded) {
+    await new Promise((resolve, reject) => {
+      fs.readFile(path.resolve(__dirname, `../../${config.mode}.json`), (err, data) => {
+        if (err) {
+          reject(new Error('CONFIG FILE ERROR:', err.toString()))
+        } else {
+          try {
+            loadedConfig = JSON.parse(data.toString('utf8'))
+            resolve(loadedConfig)
+          } catch (e) {
+            reject(new Error('CONFIG FILE PARSE ERROR', e.toString()))
+          }
         }
-      }
+      })
     })
-  })
+  }
   
-  // Apply config to Express
-  expressApp.config = Object.assign(config, loadedConfig)
-  expressApp.use((req, res, next) => {
-    req.config = expressApp.config
-    next()
-  })
+  if (typeof expressApp === 'undefined' || typeof expressApp.use === 'undefined') {
+    opts.loaded = true
+    return Object.assign(config, loadedConfig)
+  } else {
+    opts.loaded = true
+    // Apply config to Express
+    expressApp.config = Object.assign(config, loadedConfig)
+    expressApp.use((req, res, next) => {
+      req.config = expressApp.config
+      next()
+    })  
+  }
 
   return expressApp.config
 }
