ede62d8273a2966b4f11cee26c283b9b2ba53c36
Wietse Wind <mail@wietse.com>
2020-05-05 15:28:40 +0200

    Rename user profile page

--- src/api/v1/internal/user-profile-page.js
+++ src/api/v1/internal/user-profile-page.js
@@ -1,10 +1,14 @@
-const log = require('debug')('app:user-profile-page')
+const log = require('@src/handler/log')('app:user-profile-page')
 
 module.exports = async (params, expressApp, invoker) => {
   if (invoker !== 'web') {
     throw new Error('User profile: invoker not implemented')
   }
   // const db = expressApp.db
+
+  // wietse$xumm.me
+  // wietse+savings$xumm.me
+  // wietse+tipbot$xumm.me
 
   log('user-profile-page', params)
 
